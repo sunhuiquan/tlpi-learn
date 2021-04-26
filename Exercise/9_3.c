79acede67226bb14b810d8b07ad349a6d171f76d
sunhuiquan <sunhuiquan@yeah.net>
2021-04-26 10:59:23 +0800

    add finish CH9

--- Exercise/9_3.c
+++ Exercise/9_3.c
@@ -6,21 +6,38 @@
 #include <unistd.h>
 #include <limits.h>
 #include <string.h>
+#include <pwd.h>
 
 #define MAXLINE 4096
 
 int my_initgroups(const char *user, gid_t group);
 
+/**
+ * set-user-root-ID程序，进入后是r是实际调用的，e和s都是root，
+ * my_initgroups指定的用户名找到对应id，设置成进程的实际ID
+ */
+
 int main()
 {
+    /**
+     * get/setgroups对应的是进程的实际用户id(即调用的所属的ID)，
+     * 通过set-user-root-ID测试。
+     * 
+     * 要注意的是凭证看有效，但调用一般看所属，所谓的属主指的是实际ID 
+     */
     gid_t list[NGROUPS_MAX + 1];
-    int gid_size = getgroups(NGROUPS_MAX + 1, list);
+    int gid_size;
+
+    printf("now real user ID: %d\n", getuid());
+    gid_size = getgroups(NGROUPS_MAX + 1, list);
     for (int i = 0; i < gid_size; ++i)
         printf("%u  \n", list[i]);
-    printf("============================\n");
 
-    my_initgroups("water", 1000);
+    // 永远要做错误检查，节约debug的时间太多了
+    if (my_initgroups("water", 1000) < 0)
+        printf("my_initgroups error: %s\n", strerror(errno));
 
+    printf("now real user ID: %d\n", getuid());
     gid_size = getgroups(NGROUPS_MAX + 1, list);
     for (int i = 0; i < gid_size; ++i)
         printf("%u  \n", list[i]);
@@ -30,12 +47,24 @@ int main()
 
 int my_initgroups(const char *user, gid_t group)
 {
+    struct passwd *passwd;
+    int real_id;
     FILE *fp;
     char line[MAXLINE];
     int n_group = 0;
     gid_t grouplist[NGROUPS_MAX + 1]; // plus one for formal GID
 
-    if ((fp = fopen("/etc/group", "r")) == NULL)
+    passwd = getpwnam(user);
+    if (passwd == NULL)
+        return -1;
+
+    real_id = passwd->pw_uid;
+    setreuid(real_id, -1);
+    /**
+     *  r        e    s
+     *  real_id  0    0
+     */
+    if ((fp = fopen("/etc/group", "r")) == NULL) // e_uid是root权限没问题
         return -1;
 
     while (fgets(line, MAXLINE, fp))
