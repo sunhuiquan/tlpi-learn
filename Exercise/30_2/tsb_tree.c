2f63d14e6e72250f9145d8be2997b09b77a6d9c8
sunhuiquan <sunhuiquan@yeah.net>
2021-05-13 20:16:06 +0800

    add 30_2

--- Exercise/30_2/tsb_tree.c
+++ Exercise/30_2/tsb_tree.c
@@ -1,143 +1,95 @@
 #include "tsb_tree.h"
 
-/* It's  a totally thread safe function, for there's no shared varible */
+#define RET_CHECK(r)   \
+    do                 \
+    {                  \
+        if (r != 0)    \
+        {              \
+            errno = r; \
+            return -1; \
+        }              \
+    } while (0);
+
+/* It's  a totally thread safe function, for we only can initialize one time for a tree */
 int tsb_initialize(ptsbTree tree)
 {
-    int r; // We use errno to tell wrong hence set the
-    // errno as r thougn it will reduce effective.
+    // Use errno to fint wrong and set the errno as r thougn it will reduce effective.
+    int r = pthread_mutex_init(&tree->mutex, NULL);
+    RET_CHECK(r);
+
     tree = (ptsbTree)malloc(sizeof(struct tsbTree));
     if (tree == 0)
         return -1;
 
     tree->key = NULL;
     tree->value = NULL;
-    tree->left_ptsb = NULL;
-    tree->right_ptsb = NULL;
-
-    r = pthread_mutex_init(&tree->mutex, NULL);
-    if (r != 0)
-    {
-        errno = r;
-        return -1;
-    }
+    tree->left_ptsb = tree->right_ptsb = NULL;
 
     return 0;
 }
 
 int tsb_add(ptsbTree tree, char *key, void *value)
 {
-    int r;
-    if (key == NULL)
+    if (tree == NULL || key == NULL)
     {
         errno = EINVAL;
         return -1;
     }
 
-    r = pthread_mutex_lock(&tree->mutex);
-    if (r != 0)
-    {
-        errno = r;
-        return -1;
-    }
+    int r = pthread_mutex_lock(&tree->mutex);
+    RET_CHECK(r);
+
     if (tree->key == NULL)
     {
         tree->key = key;
         tree->value = value;
+        r = pthread_mutex_unlock(&tree->mutex);
+        RET_CHECK(r);
         return 0;
     }
+
     r = pthread_mutex_unlock(&tree->mutex);
-    if (r != 0)
-    {
-        errno = r;
-        return -1;
-    }
+    RET_CHECK(r);
 
     ptsbTree pnode = (ptsbTree)malloc(sizeof(struct tsbTree));
+    if (pnode == NULL)
+        return -1;
     pnode->key = key;
     pnode->value = value;
-    pnode->left_ptsb = NULL;
-    pnode->right_ptsb = NULL;
-    r = pthread_mutex_init(&pnode->mutex, NULL);
-    if (r != 0)
-    {
-        errno = r;
-        return -1;
-    }
+    pnode->left_ptsb = pnode->right_ptsb = NULL;
+    r = pthread_mutex_init(&tree->mutex, NULL);
+    RET_CHECK(r);
 
-    ptsbTree p = tree;
-    while (1)
+    ptsbTree pre;
+    while (tree)
     {
-        r = pthread_mutex_lock(&p->mutex);
-        if (r != 0)
+        r = pthread_mutex_lock(&tree->mutex);
+        RET_CHECK(r);
+
+        int compare = strcmp(tree->key, key);
+        if (compare > 0)
         {
-            errno = r;
-            return -1;
+            pre = tree;
+            tree = tree->left_ptsb;
+            if (tree == NULL)
+                pre->left_ptsb = pnode;
         }
-
-        if (strncmp(p->key, pnode->key, MAXLINE) == 1) // To avoid C-str terminates without null character
+        else if (compare < 0)
         {
-            if (p->left_ptsb)
-                p = p->left_ptsb;
-            else
-            {
-                p->left_ptsb = pnode;
-                break;
-            }
+            pre = tree;
+            tree = tree->right_ptsb;
+            if (tree == NULL)
+                pre->right_ptsb = pnode;
         }
         else
         {
-            if (p->right_ptsb)
-                p = p->right_ptsb;
-            else
-            {
-                p->right_ptsb = pnode;
-                break;
-            }
+            r = pthread_mutex_unlock(&tree->mutex);
+            RET_CHECK(r);
+            break;
         }
 
-        r = pthread_mutex_unlock(&p->mutex);
-        if (r != 0)
-        {
-            errno = r;
-            return -1;
-        }
-    }
-
-    return 0;
-}
-
-int tsb_delete(ptsbTree tree, char *key)
-{
-    if (key == NULL)
-    {
-        errno = EINVAL;
-        return -1;
-    }
-
-    ptsbTree p = tree, del_node = NULL, cur_node = NULL;
-    int compare;
-    while (p)
-    {
-        compare = strncmp(p->key, key, MAXLINE);
-        if (compare == 0)
-        {
-            del_node = p;
-            // to do : if(){}
-        }
-        else if (compare == 1)
-        {
-            p = p->left_ptsb;
-        }
-        else
-        {
-            p = p->right_ptsb;
-        }
-    }
-
-    if (del_node == NULL)
-    {
-        // to do errno
-        return -1;
+        r = pthread_mutex_unlock(&tree->mutex);
+        RET_CHECK(r);
     }
 
     return 0;
