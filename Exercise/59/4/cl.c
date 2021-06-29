1d7d8f57ed66422f049b541f9ed357a1c11a836b
sunhuiquan <sunhuiquan@yeah.net>
2021-06-29 11:51:17 +0800

    add

--- Exercise/59/4/cl.c
+++ Exercise/59/4/cl.c
@@ -1,1 +1,55 @@
+#include <tlpi_hdr.h>
+#include "header/kv.h"
+#include "header/myinet.h"
+#include "../../../tlpi-dist/sockets/read_line.h"
 
+#define MAXLINE 1024
+
+int main(int argc, char *argv[])
+{
+	int clfd;
+	char bufline[MAXLINE];
+	ssize_t n;
+	char command[MAXLINE];
+
+	if (argc != 3)
+		printf("usage: <host> <port>");
+
+	if ((clfd = inetConnect(argv[1], argv[2], SOCK_STREAM)) == -1)
+		errExit("inetConnect");
+
+	for (;;)
+	{
+		printf("command:\n\t1.add <key> <value>\n\t2.delete <key>\n\t"
+			   "3.change <key> <value>\n\t4.show\n\t5.quit\n");
+
+		n = readLine(STDIN_FILENO, command, MAXLINE);
+		if (n < 0)
+			errExit("readLine");
+		else if (n == 0)
+			continue;
+
+		if (!strncmp(command, "add", 3) || !strncmp(command, "delete", 6) ||
+			!strncmp(command, "change", 6) || !strncmp(command, "show", 4))
+		{
+			if (write(clfd, command, strlen(command)) != strlen(command)) // 不含'\0'
+				errExit("write");
+		}
+		else if (!strncmp(command, "quit", 4))
+		{
+			break;
+		}
+		else
+		{
+			printf("wrong input, repeat again\n");
+			continue;
+		}
+
+		if ((n = readLine(clfd, bufline, MAXLINE)) < 0)
+			errExit("readLine");
+		if (n)
+			printf("%s", bufline);
+	}
+
+	return 0;
+}
--- Exercise/59/4/sv.c
+++ Exercise/59/4/sv.c
@@ -1,37 +1,96 @@
 #include <tlpi_hdr.h>
 #include "header/kv.h"
+#include "header/myinet.h"
+#include "../../../tlpi-dist/sockets/read_line.h"
+
+#define BACKLOG 5
+#define MAXLINE 1024
+
+kv_dsa *kv;
+
+int handle_command(char *command, char *result);
 
 int main()
 {
-	/* test for kv library */
-	// kv_dsa *kv, *p;
+	int svfd, confd;
+	struct sockaddr_storage claddr;
+	socklen_t addrlen;
+	char command[MAXLINE];
+	char bufline[MAXLINE];
+	ssize_t n;
+	char addrStr[IS_ADDR_STR_LEN];
 
-	// if ((kv = init_kv()) == NULL)
-	// 	errExit("init_kv");
-	// if (add_kv(kv, "apple", "pingguo") == -1)
-	// 	errExit("add_kv");
-	// if (add_kv(kv, "banana", "xiangjiao") == -1)
-	// 	errExit("add_kv");
-	// if (add_kv(kv, "canada", "jianada") == -1)
-	// 	errExit("add_kv");
-	// if (delete_kv(kv, "canada") == -1)
-	// 	errExit("delete");
-	// if (delete_kv(kv, "apple") == -1)
-	// 	errExit("delete");
-	// if (add_kv(kv, "japan", "riben") == -1)
-	// 	errExit("add_kv");
-	// if (add_kv(kv, "rust", "pangxie") == -1)
-	// 	errExit("add_kv");
-	// if (delete_kv(kv, "rust") == -1)
-	// 	errExit("delete");
-	// if (change_kv(kv, "banana", "a") == -1)
-	// 	errExit("change_kv");
-	// if (change_kv(kv, "japan", "b") == -1)
-	// 	errExit("change_kv");
+	if ((kv = init_kv()) == NULL)
+		errExit("init_kv");
 
-	// for (p = kv->next; p; p = p->next)
-	// 	printf("(%s : %s)\n", p->key, p->value);
-	/* end test for kv library*/
+	if ((svfd = inetListen("50000", BACKLOG, NULL)) == -1)
+		errExit("inetListen");
+
+	for (;;)
+	{
+		addrlen = sizeof(struct sockaddr_storage);
+		if ((confd = accept(svfd, (struct sockaddr *)&claddr, &addrlen)) == -1)
+		{
+			errMsg("accept fail");
+			continue;
+		}
+		printf("connect from: %s\n", inetAddressStr((struct sockaddr *)&claddr,
+													addrlen, addrStr, IS_ADDR_STR_LEN));
+
+		if ((n = readLine(confd, command, MAXLINE)) <= 0)
+		{
+			errMsg("readLine fail");
+			close(confd);
+			continue;
+		}
+		printf("command: %s", command);
+
+		handle_command(command, bufline);
+
+		strcpy(bufline, command);
+
+		if (write(confd, bufline, n) != n)
+			errMsg("write fail");
+		close(confd);
+	}
 
 	return 0;
+}
+
+int handle_command(char *command, char *result)
+{
+	char temp[MAXLINE];
+	strncpy(temp, command, MAXLINE);
+	temp[strlen(temp) - 1] = ' '; // '\n' 变 ' '
+	char *words[4];
+	char *ptemp = temp, *p;
+
+	int i = 0;
+	for (; i < 3; ++i)
+	{
+		p = strchr(ptemp, ' ');
+		if (p == NULL)
+			break;
+
+		*p = '\0';
+		words[i] = ptemp;
+		ptemp = p + 1;
+	}
+
+	if (!strcmp(words[0], "add") && i == 3)
+		return add_kv(kv, words[1], words[2]);
+	else if (!strcmp(words[0], "delete") && i == 2)
+		return delete_kv(kv, words[1]);
+	else if (!strcmp(words[0], "change") && i == 3)
+		return change_kv(kv, words[1], words[2]);
+	else if (!strcmp(words[0], "show") && i == 1)
+	{
+		kv_dsa *p = kv;
+		for (; p; p = p->next)
+			sprintf(result, "(%s : %s)\n", p->key, p->value);
+		return 0;
+	}
+
+	sprintf(result, "invalid command\n");
+	return -1;
 }
