1ecb57b1b9ab3dabed7c48085d1420cbf2821b3c
sunhuiquan <sunhuiquan@yeah.net>
2021-07-27 00:16:52 +0800

    add 18_7

--- Exercise/18/7/18_7.c
+++ Exercise/18/7/18_7.c
@@ -1,7 +1,38 @@
+#define _XOPEN_SOURCE 600
 #include <tlpi_hdr.h>
+#include <ftw.h>
 
-int main()
+static int regular_file = 0;
+static int dir_file = 0;
+static int sym_link = 0;
+
+int count_file(const char *pathname, const struct stat *statbuf, int typeflag, struct FTW *ftwbuf)
 {
+	if (S_ISREG(statbuf->st_mode))
+		++regular_file;
+	if (S_ISDIR(statbuf->st_mode))
+		++dir_file;
+	if (S_ISLNK(statbuf->st_mode))
+		++sym_link;
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int flag = 0;
+	if (argc != 2 || !strcmp(argv[1], "--help"))
+	{
+		printf("usage: dir\n");
+		exit(EXIT_SUCCESS);
+	}
+
+	flag |= FTW_PHYS; // 不对符号链接解引用，才可以看到符号链接本身
+	if (nftw(argv[1], count_file, 10, flag) == -1)
+		errExit("nftw");
+
+	printf("Regular files takes about %.2lf%%\n", (double)regular_file / (regular_file + dir_file + sym_link) * 100);
+	printf("Dirent  files takes about %.2lf%%\n", (double)dir_file / (regular_file + dir_file + sym_link) * 100);
+	printf("symbol  links takes about %.2lf%%\n", (double)sym_link / (regular_file + dir_file + sym_link) * 100);
 
 	return 0;
 }
