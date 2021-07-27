9f73cbcfa75ef37b0cddec23527544c2a6cbe84c
sunhuiquan <sunhuiquan@yeah.net>
2021-07-27 13:06:22 +0800

    add

---
+++ Exercise/18/8/18_8.c
@@ -0,0 +1,118 @@
+/*	An easy implementation of nftw()
+	using opendir(), readdir(), closedir(), stat()
+*/
+
+#define _XOPEN_SOURCE 600
+#include <tlpi_hdr.h>
+#include <ftw.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <limits.h>
+
+typedef int (*func_t)(const char *filename,
+					  const struct stat *status, int flag,
+					  struct FTW *info);
+
+int _ftw_rec(const char *pathname, func_t func, int descriptors, int flag, int level)
+{
+	int ret_value = 0;
+	DIR *dir;
+	struct dirent *dirent;
+	struct stat sbuf;
+	char path[PATH_MAX] = "";
+	char temp[PATH_MAX];
+	char cwd[PATH_MAX];
+
+	if (flag & FTW_CHDIR)
+	{
+		if ((getcwd(cwd, PATH_MAX)) == NULL)
+			return -1;
+		if (chdir(pathname) == -1)
+			return -1;
+	}
+	else
+	{
+		if (pathname[strlen(pathname) - 1] != '/')
+			sprintf(path, "%s/", pathname); // 这里是为了确保有个'/'，因为'/////'都等同于一个'/'没关系
+		else
+			sprintf(path, "%s", pathname); // 这里是为了确保有个'/'，因为'/////'都等同于一个'/'没关系
+	}
+
+	if ((dir = opendir(pathname)) == NULL)
+		return -1;
+	--descriptors;
+	if (descriptors < 0)
+		return -1;
+	while (1)
+	{
+		errno = 0;
+		if ((dirent = readdir(dir)) == NULL)
+		{
+			if (errno)
+				continue;
+			break;
+		}
+
+		strcpy(temp, path);
+		if (!strcmp(temp, "./") && !strcmp(dirent->d_name, "."))
+			strcpy(temp, ".");
+		else
+			strncat(temp, dirent->d_name, PATH_MAX);
+		if (flag & FTW_PHYS)
+		{
+			if (lstat(temp, &sbuf) == -1)
+				return -1;
+		}
+		else if (stat(temp, &sbuf) == -1)
+			return -1;
+
+		if (strcmp(dirent->d_name, ".."))
+		{
+			if (S_ISDIR(sbuf.st_mode))
+			{
+				if (strlen(temp) > 2 && temp[strlen(temp) - 1] == '.' && temp[strlen(temp) - 2] == '/')
+				{
+					if (!(level == 0 && temp[0] == '/' && *dirent->d_name == '.'))
+						if ((temp[strlen(temp) - 3] != '.' || temp[strlen(temp) - 4] != '.'))
+							continue;
+				}
+
+				if (func(temp, &sbuf, 0, NULL) != 0)
+				{
+					if (flag & FTW_CHDIR)
+						if (chdir(cwd) == -1)
+							return -1;
+					return ret_value;
+				}
+
+				if (strcmp(dirent->d_name, "."))
+				{
+					if ((ret_value = _ftw_rec(temp, func, descriptors, flag, level + 1)) != 0)
+					{
+						if (flag & FTW_CHDIR)
+							if (chdir(cwd) == -1)
+								return -1;
+						return ret_value;
+					}
+				}
+			}
+			else if (func(temp, &sbuf, 0, NULL) != 0)
+			{
+				if (flag & FTW_CHDIR)
+					if (chdir(cwd) == -1)
+						return -1;
+				return ret_value;
+			}
+		}
+	}
+	if (flag & FTW_CHDIR)
+		if (chdir(cwd) == -1)
+			return -1;
+	closedir(dir);
+	return 0;
+}
+
+int mynftw(const char *dir, func_t func, int descriptors, int flag)
+{
+	return _ftw_rec(dir, func, descriptors, flag, 0);
+}
---
+++ Exercise/18/8/right.c
@@ -0,0 +1,41 @@
+#define _XOPEN_SOURCE 600
+#include <tlpi_hdr.h>
+#include <ftw.h>
+
+int mynftw(const char *__dir, __nftw_func_t __func, int __descriptors, int __flag);
+
+static int regular_file = 0;
+static int dir_file = 0;
+static int sym_link = 0;
+
+int count_file(const char *pathname, const struct stat *statbuf, int typeflag, struct FTW *ftwbuf)
+{
+	// printf("%s\n", pathname);
+	if (S_ISREG(statbuf->st_mode))
+		++regular_file;
+	if (S_ISDIR(statbuf->st_mode))
+		++dir_file;
+	if (S_ISLNK(statbuf->st_mode))
+		++sym_link;
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int flag = 0;
+	if (argc != 2 || !strcmp(argv[1], "--help"))
+	{
+		printf("usage: dir\n");
+		exit(EXIT_SUCCESS);
+	}
+
+	flag |= FTW_PHYS; // 不对符号链接解引用，才可以看到符号链接本身
+	if (nftw(argv[1], count_file, 10, flag) == -1)
+		errExit("nftw");
+
+	printf("Regular files takes about %.4lf%%\n", (double)regular_file / (regular_file + dir_file + sym_link) * 100);
+	printf("Dirent  files takes about %.4lf%%\n", (double)dir_file / (regular_file + dir_file + sym_link) * 100);
+	printf("symbol  links takes about %.4lf%%\n", (double)sym_link / (regular_file + dir_file + sym_link) * 100);
+
+	return 0;
+}
