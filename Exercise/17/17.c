5174e4bba93173ab2cd66612856eaacb2326068c
sunhuiquan <sunhuiquan@yeah.net>
2021-08-22 00:09:13 +0800

    add

--- Exercise/17/17.c
+++ Exercise/17/17.c
@@ -5,12 +5,43 @@
 #include <errno.h>
 #include <tlpi_hdr.h>
 
+int get_perms(char *perms, acl_entry_t *entry, acl_permset_t *permset, int permVal)
+{
+	if (acl_get_permset(*entry, permset) == -1)
+		return -1;
+
+	permVal = acl_get_perm(*permset, ACL_READ);
+	if (permVal == -1)
+		return -1;
+	fprintf(perms, "%s %c", perms, (permVal == 1) ? 'r' : '-');
+	permVal = acl_get_perm(*permset, ACL_WRITE);
+	if (permVal == -1)
+		return -1;
+	fprintf(perms, "%s %c", perms, (permVal == 1) ? 'w' : '-');
+	permVal = acl_get_perm(*permset, ACL_EXECUTE);
+	if (permVal == -1)
+		return -1;
+	fprintf(perms, "%s %c\n", perms, (permVal == 1) ? 'x' : '-');
+}
+
 int main(int argc, char *argv[])
 {
 	int is_user = 0;
 	char *name;
 	struct passwd *pwd;
 	struct group *grp;
+	acl_t acl;
+	acl_type_t type;
+	acl_entry_t entry;
+	acl_tag_t tag;
+	uid_t *uidp;
+	gid_t *gidp;
+	acl_permset_t permset;
+	char *acl_name;
+	int entryId, permVal, opt;
+	char perms[10] = "";
+	char obj_perms[10] = "";
+	int is_find = 0;
 
 	if (argc != 3 || !strcmp(argv[1], "-help"))
 	{
@@ -64,6 +95,77 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%s\n", name);
+
+	// ------------------------------------------------------------------
+
+	acl = acl_get_file(argv[3], type);
+	if (acl == NULL)
+		errExit("acl_get_file");
+
+	for (entryId = ACL_FIRST_ENTRY;; entryId = ACL_NEXT_ENTRY)
+	{
+
+		if (acl_get_entry(acl, entryId, &entry) != 1)
+			break;
+
+		if (acl_get_tag_type(entry, &tag) == -1)
+			errExit("acl_get_tag_type");
+
+		if (is_user && tag == ACL_USER)
+		{
+			uidp = acl_get_qualifier(entry);
+			if (uidp == NULL)
+				errExit("acl_get_qualifier");
+
+			pwd = getpwuid(uidp);
+			if (pwd == NULL)
+				errExit("getpwuid errno or can't find that user");
+
+			if (acl_free(uidp) == -1)
+				errExit("acl_free");
+
+			if (!strcmp(pwd->pw_name, name))
+			{
+				if (get_perms(perms, &entry, &permset, permVal) == -1)
+					errExit("get_perms");
+				is_find = 1;
+				break;
+			}
+		}
+		else if (!is_user && tag == ACL_GROUP)
+		{
+			gidp = acl_get_qualifier(entry);
+			if (gidp == NULL)
+				errExit("acl_get_qualifier");
+
+			grp = getgrgid(atoi(argv[2]));
+			if (grp == NULL)
+				errExit("getpwuid errno or can't find that user");
+
+			if (acl_free(gidp) == -1)
+				errExit("acl_free");
+
+			if (!strcmp(grp->gr_name, name))
+			{
+				if (get_perms(perms, &entry, &permset, permVal) == -1)
+					errExit("get_perms");
+				is_find = 1;
+				break;
+			}
+		}
+
+		if (tag == ACL_USER_OBJ || tag == ACL_GROUP_OBJ)
+			if (get_perms(obj_perms, &entry, &permset, permVal) == -1)
+				errExit("get_perms");
+	}
+
+	if (acl_free(acl) == -1)
+		errExit("acl_free");
+
+	if (is_find)
+		printf("%s\n", perms);
+	else
+		printf("%s\n", obj_perms);
 
 	return 0;
 }
