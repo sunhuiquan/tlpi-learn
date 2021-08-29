6ef460eb36f7fef0d10588a62be2538b74a51b9b
sunhuiquan <sunhuiquan@yeah.net>
2021-08-29 13:30:05 +0800

    add 42

--- Exercise/42/shared_demo/main.c
+++ Exercise/42/shared_demo/main.c
@@ -1,11 +1,13 @@
 // 和头文件的作用一样，一般写在头文件里来可重用
 void mod1();
 void mod2();
+void mod3();
 
 int main()
 {
 	mod1();
 	mod2();
+	mod3();
 
 	return 0;
 }
---
+++ Exercise/42/shared_demo/mod3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void mod3()
+{
+	printf("This is mod3 and shared library file is in /usr/local/lib.\n");
+}
