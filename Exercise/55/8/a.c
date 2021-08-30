45670860f69eaae5dec00e6b31e4a9e71178db54
sunhuiquan <sunhuiquan@yeah.net>
2021-08-30 19:55:52 +0800

    add

---
+++ Exercise/55/8/a.c
@@ -0,0 +1,73 @@
+// a -> b
+#include <sys/file.h>
+#include <fcntl.h>
+#include <time.h>
+#include "tlpi_hdr.h"
+
+#define BUF_SIZE 1000
+
+char *currTime(const char *format);
+
+int main(int argc, char *argv[])
+{
+	int fd1, fd2;
+
+	fd1 = open("./a", O_RDONLY);
+	if (fd1 == -1)
+		errExit("open");
+
+	fd2 = open("./b", O_RDONLY);
+	if (fd2 == -1)
+		errExit("open");
+
+	printf("PID %ld: requesting file a at %s\n", (long)getpid(),
+		   currTime("%T"));
+	if (flock(fd1, LOCK_EX) == -1)
+	{
+		if (errno == EWOULDBLOCK)
+			fatal("PID %ld: already locked file a !", (long)getpid());
+		else
+			errExit("File a flock (PID=%ld)", (long)getpid());
+	}
+	sleep(5);
+
+	printf("PID %ld: requesting file b at %s\n", (long)getpid(),
+		   currTime("%T"));
+	if (flock(fd2, LOCK_EX) == -1)
+	{
+		if (errno == EWOULDBLOCK)
+			fatal("PID %ld: already locked file b !", (long)getpid());
+		else
+			errExit("File b flock (PID=%ld)", (long)getpid());
+	}
+	sleep(5);
+
+	if (flock(fd2, LOCK_UN) == -1)
+		errExit("flock");
+	printf("PID %ld: released file b at %s\n", (long)getpid(),
+		   currTime("%T"));
+
+	if (flock(fd1, LOCK_UN) == -1)
+		errExit("flock");
+	printf("PID %ld: released file a at %s\n", (long)getpid(),
+		   currTime("%T"));
+
+	exit(EXIT_SUCCESS);
+}
+
+char *currTime(const char *format)
+{
+	static char buf[BUF_SIZE]; /* Nonreentrant */
+	time_t t;
+	size_t s;
+	struct tm *tm;
+
+	t = time(NULL);
+	tm = localtime(&t);
+	if (tm == NULL)
+		return NULL;
+
+	s = strftime(buf, BUF_SIZE, (format != NULL) ? format : "%c", tm);
+
+	return (s == 0) ? NULL : buf;
+}
