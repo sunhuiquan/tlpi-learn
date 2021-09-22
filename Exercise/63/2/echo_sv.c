225759a94ff54cae03d6de1534f8a080ba0afc8a
sunhuiquan <sunhuiquan@yeah.net>
2021-09-22 11:36:21 +0800

    add

--- Exercise/63/2/echo_sv.c
+++ Exercise/63/2/echo_sv.c
@@ -1,11 +1,17 @@
+/* 一个使用I/O多路复用技术的并发服务器模型 */
+
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <tlpi_hdr.h>
 
 int main()
 {
 	int lfd, dfd; // listen fd and datagram fd
 	struct sockaddr_in addr;
+
+	int nfds, nready;
+	fd_set rfdset;
 
 	// create a listening fd
 	if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
@@ -30,6 +36,36 @@ int main()
 		errExit("bind");
 
 	// add these socket fds into select's rfdset list
+	nfds = max(lfd, dfd) + 1;
+	FD_ZERO(&rfdset);
+	FD_SET(lfd, &rfdset);
+	FD_SET(dfd, &rfdset);
+
+	while (true)
+	{
+		nready = select(nfds, &rfdset, NULL, NULL, NULL);
+		if (nready == -1)
+			errExit("select");
+
+		// 为了防止因为循环读不停到来的数据导致其他fd饥饿，我们每次只做适量的I/O操作
+		if (FD_ISSET(lfd, &rfdset)) // can accept and create connected fd(s)
+		{
+			// to do
+
+			--nready;
+			if (!nready)
+				continue;
+		}
+
+		if (FD_ISSET(dfd, &rfdset)) // can read the datagram fd
+		{
+			--nready;
+			if (!nready)
+				continue;
+		}
+
+		// for (int i = 0;;++i) // the rest of tcp connected fds
+	}
 
 	return 0;
 }
