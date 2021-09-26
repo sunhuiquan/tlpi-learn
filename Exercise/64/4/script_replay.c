36b0f72f7fe2fcf68d052c2203ef13c99ad773f1
sunhuiquan <sunhuiquan@yeah.net>
2021-09-26 21:01:29 +0800

    add finish 64-4

--- Exercise/64/4/script_replay.c
+++ Exercise/64/4/script_replay.c
@@ -56,7 +56,6 @@ int main(int argc, char *argv[])
 	pid_t childPid;
 	time_t curr_time;
 	char *pctime;
-	char time_str[MAXTIMELEN];
 	struct sigaction act;
 	int maxfd;
 	long cts;
@@ -64,7 +63,8 @@ int main(int argc, char *argv[])
 	int index;
 	int pfd[2];
 	char *pbuf, command[BUF_SIZE];
-	int readn;
+	int readn, closed_replay_select = 0;
+	char ch;
 
 	/* Retrieve the attributes of terminal on which we are started */
 
@@ -89,7 +89,7 @@ int main(int argc, char *argv[])
 		if (time(&curr_time) == -1)
 			errExit("time");
 		pctime = ctime(&curr_time);
-		printf("Script started on %s\n", (pctime == NULL) ? "can't get time" : pctime);
+		printf("Use 'q' to quit and script started on %s\n", (pctime == NULL) ? "can't get time" : pctime);
 
 		/* If the SHELL variable is set, use its value to determine
            the shell execed in child. Otherwise use /bin/sh. */
@@ -190,8 +190,10 @@ int main(int argc, char *argv[])
 	for (;;)
 	{
 		FD_ZERO(&inFds);
-		FD_SET(replayFd, &inFds);
+		if (closed_replay_select == 0)
+			FD_SET(replayFd, &inFds);
 		FD_SET(masterFd, &inFds);
+		FD_SET(STDIN_FILENO, &inFds);
 
 		if (select(maxfd + 1, &inFds, NULL, NULL, NULL) == -1)
 		{
@@ -204,18 +206,26 @@ int main(int argc, char *argv[])
 		if (FD_ISSET(replayFd, &inFds))
 		{ /* stdin --> pty */
 			numRead = read(replayFd, buf, BUF_SIZE);
-			if (numRead <= 0) // 正常退出^D也是通过这个途径
-				exit(EXIT_SUCCESS);
-
-			for (int i = 0; i < numRead; ++i)
+			if (numRead < 0) // 正常退出^D也是通过这个途径
+				errExit("read");
+			else if (numRead == 0)
+			{ // 读到文件结束会读到一个EOF告知
+				close(pfd[1]);
+				closed_replay_select = 1;
+				FD_CLR(replayFd, &inFds);
+			}
+			else
 			{
-				buf2[index++] = buf[i];
-				if (buf[i] == '\n')
+				for (int i = 0; i < numRead; ++i)
 				{
-					buf2[index] = '\0'; // 这样strlen就能计算了
-					if (write(pfd[1], buf2, strlen(buf2)) != strlen(buf2))
-						errExit("write");
-					index = 0;
+					buf2[index++] = buf[i];
+					if (buf[i] == '\n')
+					{
+						buf2[index] = '\0'; // 这样strlen就能计算了
+						if (write(pfd[1], buf2, strlen(buf2)) != strlen(buf2))
+							errExit("write");
+						index = 0;
+					}
 				}
 			}
 		}
@@ -223,22 +233,22 @@ int main(int argc, char *argv[])
 		if (FD_ISSET(masterFd, &inFds))
 		{ /* pty --> stdout+file */
 			numRead = read(masterFd, buf, BUF_SIZE);
-			if (numRead <= 0) // 正常退出也是通过这个途径
-			{
-				if (time(&curr_time) == -1)
-					errExit("time");
-				pctime = ctime(&curr_time);
-				snprintf(time_str, MAXTIMELEN, "Script started on %s\n", (pctime == NULL) ? "can't get time" : pctime);
-				printf("%s\n", time_str);
-				if (write(scriptFd, time_str, strlen(time_str)) != strlen(time_str))
-					fatal("partial/failed write (scriptFd)");
-				exit(EXIT_SUCCESS);
-			}
+			if (numRead <= 0) // 因为这个重放不能通过EOF退出
+				errExit("read");
 
 			if (write(STDOUT_FILENO, buf, numRead) != numRead)
 				fatal("partial/failed write (STDOUT_FILENO)");
 			if (write(scriptFd, buf, numRead) != numRead)
 				fatal("partial/failed write (scriptFd)");
+		}
+
+		if (FD_ISSET(STDIN_FILENO, &inFds))
+		{
+			numRead = read(STDIN_FILENO, &ch, 1);
+			if (numRead < 0)
+				errExit("read");
+			if (ch == 'q')
+				exit(EXIT_SUCCESS);
 		}
 	}
 }
